8049e8694ee7f20cdcc5fb0328c889c393258658
metaleap <metaleap@users.noreply.github.com>
2018-01-23 20:04:39 +0100

    [autopush] «May not be combined with other discounts.»

--- cmd.go
+++ cmd.go
@@ -4,16 +4,54 @@ import (
 	"bytes"
 	"os/exec"
 	"strings"
+	"sync"
 )
 
+// CmdTry is used for `CmdsTryStart` to probe in parallel the installation status of multiple programs.
+type CmdTry struct {
+	Args []string
+	Ran  *bool
+}
+
+// CmdTryStart attempts to run the specified program, records whether this resulted in an `error`, and immediately kills the program before returning.
+func CmdTryStart(cmdname string, cmdargs ...string) (err error) {
+	cmd := exec.Command(cmdname, cmdargs...)
+	err = cmd.Start()
+	defer cmd.Wait()
+	if cmd.Process != nil {
+		_ = cmd.Process.Kill()
+	}
+	return
+}
+
+// CmdsTryStart calls `CmdTryStart` for all specified `cmds` in parallel.
+func CmdsTryStart(cmds map[string]*CmdTry) {
+	var w sync.WaitGroup
+	run := func(cmd string, try *CmdTry) {
+		defer w.Done()
+		*try.Ran = (nil == CmdTryStart(cmd, try.Args...))
+	}
+	w.Add(len(cmds))
+	for cmdname, cmdmore := range cmds {
+		go run(cmdname, cmdmore)
+	}
+	w.Wait()
+}
+
+// CmdExecStdin runs the specified program,
+// returning its full `stdout` response and whitespace-trimmed `stderr` response.
 func CmdExec(cmdName string, cmdArgs ...string) (stdout string, stderr string, err error) {
 	return CmdExecStdin("", "", cmdName, cmdArgs...)
 }
 
+// CmdExecStdin runs the specified program in the specified `dir` (if any),
+// returning its full `stdout` response and whitespace-trimmed `stderr` response.
 func CmdExecIn(dir string, cmdName string, cmdArgs ...string) (stdout string, stderr string, err error) {
 	return CmdExecStdin("", dir, cmdName, cmdArgs...)
 }
 
+// CmdExecStdin runs the specified program with the specified `stdin` (if any),
+// returning its full `stdout` response and whitespace-trimmed `stderr` response.
 func CmdExecStdin(stdin string, dir string, cmdName string, cmdArgs ...string) (stdout string, stderr string, err error) {
 	if cmdName != "" && strings.Contains(cmdName, " ") && len(cmdArgs) == 0 {
 		cmdArgs = strings.Split(cmdName, " ")
--- readme.md
+++ readme.md
@@ -10,18 +10,39 @@
 ```go
 func CmdExec(cmdName string, cmdArgs ...string) (stdout string, stderr string, err error)
 ```
+CmdExecStdin runs the specified program, returning its full `stdout` response
+and whitespace-trimmed `stderr` response.
 
 #### func  CmdExecIn
 
 ```go
 func CmdExecIn(dir string, cmdName string, cmdArgs ...string) (stdout string, stderr string, err error)
 ```
+CmdExecStdin runs the specified program in the specified `dir` (if any),
+returning its full `stdout` response and whitespace-trimmed `stderr` response.
 
 #### func  CmdExecStdin
 
 ```go
 func CmdExecStdin(stdin string, dir string, cmdName string, cmdArgs ...string) (stdout string, stderr string, err error)
 ```
+CmdExecStdin runs the specified program with the specified `stdin` (if any),
+returning its full `stdout` response and whitespace-trimmed `stderr` response.
+
+#### func  CmdTryStart
+
+```go
+func CmdTryStart(cmdname string, cmdargs ...string) (err error)
+```
+CmdTryStart attempts to run the specified program, records whether this resulted
+in an `error`, and immediately kills the program before returning.
+
+#### func  CmdsTryStart
+
+```go
+func CmdsTryStart(cmds map[string]*CmdTry)
+```
+CmdsTryStart calls `CmdTryStart` for all specified `cmds` in parallel.
 
 #### func  SetupIpcPipes
 
@@ -31,3 +52,15 @@ func SetupIpcPipes(bufferCapacity int, ipcSplitProtocol bufio.SplitFunc, needJso
 SetupIpcPipes sets up IPC pipes with the specified `bufferCapacity`. If
 `ipcSplitProtocol` is `nil`, a readline-writeline ping-pong protocol is assumed.
 Only if `needJsonOut`, is `jsonOut` allocated via `json.NewEncoder(rawOut)`.
+
+#### type CmdTry
+
+```go
+type CmdTry struct {
+	Args []string
+	Ran  *bool
+}
+```
+
+CmdTry is used for `CmdsTryStart` to probe in parallel the installation status
+of multiple programs.
