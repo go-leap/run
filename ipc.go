16389092552a19cf5e1a89fd586878ce5bcfd81f
metaleap <metaleap@users.noreply.github.com>
2019-09-08 20:23:01 +0200

    misc tweakage

--- ipc.go
+++ ipc.go
@@ -9,7 +9,7 @@ import (
 // SetupIpcPipes sets up IPC pipes with the specified `bufferCapacity`. If `ipcSplitProtocol` is `nil`, a readline-writeline ping-pong protocol is assumed. Only if `needJsonOut`, is `jsonOut` allocated via `json.NewEncoder(rawOut)`.
 func SetupIpcPipes(bufferCapacity int, ipcSplitProtocol bufio.SplitFunc, needJsonOut bool) (stdin *bufio.Scanner, rawOut *bufio.Writer, jsonOut *json.Encoder) {
 	stdin = bufio.NewScanner(os.Stdin)
-	stdin.Buffer(make([]byte, bufferCapacity), 8+bufferCapacity)
+	stdin.Buffer(make([]byte, bufferCapacity), 8+bufferCapacity /* max needs to be greater than cap to allow support for later growth-if-and-when-needed */)
 	if ipcSplitProtocol != nil {
 		stdin.Split(ipcSplitProtocol)
 	}
